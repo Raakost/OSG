7b0e3bfdb246dfd07c5aeed685b6e3f1b5d99eb8
wescorn <act.wescorn@gmail.com>
2015-11-27 11:41:33 +0100

    Added DAL test project

---
+++ OSG_REST/TestDAL/TestTrainerManager.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDAL
+{
+    [TestFixture]
+    class TestTrainerManager
+    {
+        [Test]
+        public void Test_Some_Stuff()
+        {
+        }
+    }
+}
