8fd75b6f125bfb82f9a79a2a8a01af76127bcf0c
wescorn <act.wescorn@gmail.com>
2015-11-27 10:10:18 +0100

    DTO classes created

---
+++ OSG_REST/OSG_DTO/NewsDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSG_DTO
+{
+    [DataContract]
+    public class NewsDTO
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public DateTime Date { get; set; }
+        [DataMember]
+        public string Picture { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public string Title { get; set; }
+        [DataMember]
+        public IEnumerable<CommentDTO> Comments { get; set; }
+    }
+}
