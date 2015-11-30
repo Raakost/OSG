e4f241c0b6c30b6f72ef659cfd7d2b0db8326ebb
Jeanette Sydbøge <jeanettesydboege@gmail.com>
2015-11-30 11:53:32 +0100

    Added models in OSG for comments, events, trainers and news.

---
+++ OSG/OSG/Models/ViewModel/EventCalendar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OSG.Models.ViewModel
+{
+    public class EventCalendar
+    {
+        public List<Event> Events { get; set; }
+        public List<Trainer> Trainers { get; set; }
+    }
+}
