3d7be5c672b4d61f05093ca2f07ee0f5a41f835d
Mikael Kramer <manifibell@gmail.com>
2015-11-30 11:57:00 +0100

    Added REST_API TrainerController and started to work on ReadAll() Method [HttpGet]

---
+++ OSG_REST/OSG_REST/Controllers/TrainerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DAL;
+using OSG_DTO;
+
+namespace OSG_REST.Controllers
+{
+    public class TrainerController : ApiController
+    {
+        //[HttpGet]
+        //public IEnumerable<TrainerDTO> ReadAll()
+        //{
+        //    return new Facade().GetTrainerManager().ReadAll();
+        //} 
+    }
+}
