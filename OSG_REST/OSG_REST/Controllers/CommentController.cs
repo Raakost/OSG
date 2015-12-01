e316e6f5d55c5e1873d4c89d88c43a9ec1bcaee4
Mikael Kramer <manifibell@gmail.com>
2015-12-01 12:33:52 +0100

    Implemented CommentController in REST_API

--- OSG_REST/OSG_REST/Controllers/CommentController.cs
+++ OSG_REST/OSG_REST/Controllers/CommentController.cs
@@ -1,13 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using System.Web.Http;
+using DAL;
+using DAL.DomainModel;
+using OSG_DTO;
+using OSG_DTO.Converter;
+using OSG_REST.Controllers.IController;
 
 namespace OSG_REST.Controllers
 {
-    public class CommentController : ApiController
+    public class CommentController : ApiController, IOSGController<CommentDTO>
     {
+        [HttpPost]
+        public CommentDTO Create(CommentDTO dto)
+        {
+            var comment = new Facade().GetCommentManager().Create(new CommentConverter().ConvertDTO(dto));
+            return new CommentConverter().ConvertModel(comment);
+        }
+
+        [HttpGet]
+        public CommentDTO ReadById(int id)
+        {
+            return new CommentConverter().ConvertModel(new Facade().GetCommentManager().ReadByID(id));
+        }
+
+        [HttpGet]
+        public IEnumerable<CommentDTO> ReadAll()
+        {
+            return new CommentConverter().ConvertListToDTO(new Facade().GetCommentManager().ReadAll());
+        }
+
+        [HttpPut]
+        public CommentDTO Update(int id, CommentDTO dto)
+        {
+            dto.Id = id;
+            var comment = new Facade().GetCommentManager().Update(new CommentConverter().ConvertDTO(dto));
+            return new CommentConverter().ConvertModel(comment);
+        }
+
+        [HttpDelete]
+        public bool Delete(int id)
+        {
+            var comment = new Comment() { Id = id };
+            if (new Facade().GetCommentManager().Delete(comment))
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
