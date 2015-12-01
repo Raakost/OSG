c04d2402aee53f27f48ffcbe237a783014f00076
wescorn <act.wescorn@gmail.com>
2015-12-01 12:00:49 +0100

    Gateway facade added

---
+++ OSG/Gateway/Facade/Facade.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gateway.Services;
+
+namespace Gateway.Facade
+{
+    public class Facade
+    {
+        public NewsGatewayService GetNewsGateway()
+        {
+            return new NewsGatewayService();
+        }
+
+        public TrainerGatewayService GetTrainerGateway()
+        {
+            return new TrainerGatewayService();
+        }
+
+        public CommentGatewayService GetCommentGateway()
+        {
+            return new CommentGatewayService();
+        }
+
+        public EventGatewayService GetEventGateway()
+        {
+            return new EventGatewayService();
+        }
+    }
+}
