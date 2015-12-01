715ca4c0fd44cf7783a4537ebd88dbfb7a71c17b
Jeanette Sydbøge <jeanettesydboege@gmail.com>
2015-12-01 15:51:26 +0100

    Changed jumbotron to carousel on frontpage, added login view for admin.

---
+++ OSG/OSG/Controllers/Admin/AdminEventController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OSG.Controllers.Admin
+{
+    public class AdminEventController : Controller
+    {
+        // GET: AdminEvent
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ OSG/OSG/Controllers/Admin/AdminTrainerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OSG.Controllers.Admin
+{
+    public class AdminTrainerController : Controller
+    {
+        // GET: AdminTrainer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
