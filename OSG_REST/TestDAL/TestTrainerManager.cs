a9238ec8f2eb0f06b489b66705fc42c57d07924c
Mikael Kramer <manifibell@gmail.com>
2015-12-02 09:25:04 +0100

    Added missing Nunit packages to TestDal in OSG_REST Started TrainerManager tests

--- OSG_REST/TestDAL/TestTrainerManager.cs
+++ OSG_REST/TestDAL/TestTrainerManager.cs
@@ -1,18 +1,29 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DAL.DomainModel;
 
 namespace TestDAL
 {
     [TestFixture]
     class TestTrainerManager
     {
-        [Test]
-        public void Test_Some_Stuff()
+        private Trainer _trainer;
+        [SetUp]
+        public void Init()
         {
+            _trainer = new Trainer()
+            {
+                Id = 1,
+                FirstName = "Mikkel",
+                LastName = "Madsen",
+                Email = "mikkel@mail.com",
+                PhoneNo = "22334455",
+                Description = "A description",
+            };
+        }
+        [Test]
+        public void Test_Trainer_First_Name_Property()
+        {
+            Assert.AreEqual("Mikkel", _trainer.FirstName);
         }
     }
 }
