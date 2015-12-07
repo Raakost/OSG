54c10fc637d757350dc1452cf3fea11bf772483b
Mikael Kramer <manifibell@gmail.com>
2015-12-07 11:45:53 +0100

    Made Trainer tests better and added an update test. Small fixes

--- OSG_REST/TestDAL/TestTrainerManager.cs
+++ OSG_REST/TestDAL/TestTrainerManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using DAL.DomainModel;
 using DAL.Managers;
@@ -16,8 +17,6 @@ namespace TestDAL
         [SetUp]
         public void Init()
         {
-            _trainerManager = new TrainerManager();
-            
             _trainer1 = new Trainer()
             {
                 Id = 4,
@@ -41,36 +40,65 @@ namespace TestDAL
         }
 
         [Test]
-        public void Test_ReadAll_In_TrainerManager()
+        public void Test_ReadAll_With_Count3_In_TrainerManager()
         {
-            Assert.AreEqual(3, _trainerManager.ReadAll(10).Count());
+            _trainerManager = new TrainerManager();
+            int amound = 3;
+            // The seed creates default 3+ trainers to the DB. Thats why
+            // we test with ReadAll(3).
+            Assert.AreEqual(amound, _trainerManager.ReadAll(amound).Count());
 
-            var testTrainer = _trainerManager.Create(_trainer2);
-            Assert.AreEqual(_trainer2.Id, testTrainer.Id);
-
-            Assert.AreEqual(4, _trainerManager.ReadAll(10).Count());
+            _trainer1 = _trainerManager.Create(_trainer1);
+            //amound++;
+            Assert.AreEqual(amound+1, _trainerManager.ReadAll(amound+1).Count());
         }
 
+        /// <summary>
+        /// Test if the first ID in the DB can be read with ReadByID(int number)
+        /// </summary>
         [Test]
         public void Test_ReadByID_In_TrainerManager()
         {
-            Assert.AreEqual(_trainer1.Id, _trainerManager.ReadByID(_trainer1.Id).Id);
+            _trainerManager = new TrainerManager();
+            Assert.AreEqual(1, _trainerManager.ReadByID(1).Id);
         }
 
         [Test]
         public void Test_ReadByID_In_TrainerManager_After_Create()
         {
-            _trainerManager.Create(_trainer3);
+            _trainerManager = new TrainerManager();
+            _trainer3 = _trainerManager.Create(_trainer3);
             Assert.AreEqual(_trainer3.Id, _trainerManager.ReadByID(_trainer3.Id).Id);
         }
 
         [Test]
         public void Test_Delete_Trainer()
         {
-            _trainerManager.Create(_trainer2);
+            _trainerManager = new TrainerManager();
+            _trainer2 = _trainerManager.Create(_trainer2);
             Assert.AreEqual(_trainer2.Id, _trainerManager.ReadByID(_trainer2.Id).Id);
             var IsDeleted = _trainerManager.Delete(_trainer2);
             Assert.AreEqual(true, IsDeleted);
+        }
+
+        [Test]
+        public void Test_Update_Trainer_After_Create()
+        {
+            _trainerManager = new TrainerManager();
+            var testDescription = "Create Description";
+            var _trainer4 = _trainerManager.Create(new Trainer()
+            {
+                Id = 1,
+                FirstName = "Test First Name 12",
+                Description = testDescription,
+            });
+
+            Assert.AreEqual(testDescription, _trainer4.Description);
+
+            _trainer4.Description = "New Description";
+            _trainer4 = _trainerManager.Update(_trainer4);
+
+            Assert.AreEqual(_trainer4.Description, _trainerManager.ReadByID(_trainer4.Id).Description);
         }
     }
 }
