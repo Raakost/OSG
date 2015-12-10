b2852e5f463ec661f0d6309d49d7d33b8bb0a16d
wescorn <act.wescorn@gmail.com>
2015-12-10 13:32:24 +0100

    Select Multiple trainers when creating an event

---
+++ OSG/OSG/Models/ViewModel/EventTrainers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Gateway.DomainModel;
+
+namespace OSG.Models.ViewModel
+{
+    public class EventTrainers
+    {
+
+        public EventTrainers()
+        {
+            Trainers = new List<Trainer>();
+            SelectedIDs = new List<int>();
+        }
+        public Event anEvent { get; set; }
+        public IEnumerable<Trainer> Trainers { get; set; }
+        public List<int> SelectedIDs { get; set; }
+    }
+}
